71cc92047ab6633b054f35586f89de2b80e81d9a
ruy-francisco <ruy.fsalmeida@gmail.com>
2018-07-20 11:17:02 -0300

    First Successful tests

--- gcd/src/Service/Classes/GreatestCommonDivisor.cs
+++ gcd/src/Service/Classes/GreatestCommonDivisor.cs
@@ -12,10 +12,42 @@ namespace Service.Classes
                 throw new ArgumentException("You should pass at least 2 numbers.");
             }
 
-            return 0;
+            var currentGcd = GetForTwoIntegers(numbers[0], numbers[1]);
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                currentGcd = GetForTwoIntegers(currentGcd, numbers[i]);
+            }
+
+            return currentGcd;
         }
 
-        public int GetForTwoIntegers(int first, int second) => 
-            GetForNIntegers(new int[] { first, second });
+        public int GetForTwoIntegers(int first, int second)
+        {
+            if (first == 0 || second == 0)
+            {
+                throw new ArgumentException("The intergers must to be greater than 0.");
+            }
+
+            if (first >= second)
+            {
+                var quotient = (int)first / second;
+                var remainder = (int)first % second;
+                first = second;
+                second = remainder;
+
+                while (remainder > 0)
+                {
+                    quotient = (int)first / second;
+                    remainder = (int)first % second;
+                    first = second;
+                    second = remainder;
+                }
+
+                return first;
+            }
+
+            return GetForTwoIntegers(second, first);
+        }
     }
 }
--- gcd/src/UI/Test/GreatestCommonDivisorTest.cs
+++ gcd/src/UI/Test/GreatestCommonDivisorTest.cs
@@ -1,17 +1,34 @@
 using System;
 using Xunit;
-using Moq;
 using FluentAssertions;
+using Service.Classes;
 
 namespace Test
 {
-    public class UnitTest1
+    public class GreatestCommonDivisorTest
     {
-        [Fact]
-        public void GreatestCommonDivisor_GetForNIntegers_Returns_Int()
+        [Theory]
+        [InlineData(48, 18, 6)]
+        [InlineData(48, 16, 16)]
+        [InlineData(16, 136, 8)]
+        [InlineData(55, 50, 5)]
+        public void GreatestCommonDivisor_GetForTwoIntegers_Returns_Int(int first, int second, int expectedResult)
         {
-            string name = "Ruy";
-            name.Should().Be("Ruy");
+            var gcdGenerator = new GreatestCommonDivisor();
+            var result = gcdGenerator.GetForTwoIntegers(first, second);
+            result.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 48, 18 }, 6)]
+        [InlineData(new int[] { 48, 18, 34 }, 2)]
+        [InlineData(new int[] { 56,86,38,24 }, 2)]
+        [InlineData(new int[] { 64,16,48 }, 16)]
+        public void GreatestCommonDivisor_GetForNIntegers_Returns_Int(int[] input, int expectedResult)
+        {
+            var gcdGenerator = new GreatestCommonDivisor();
+            var result = gcdGenerator.GetForNIntegers(input);
+            result.Should().Be(expectedResult);
         }
     }
 }
